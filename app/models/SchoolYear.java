411b32a3236c2e90fdefe40e290d984f0886aea7
fzheng1 <kaifenzheng@gmail.com>
2018-05-01 09:18:45 -0400

    current school year method

---
+++ app/models/SchoolYear.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class SchoolYear {
+
+
+
+    public static LocalDateTime currentSchoolYear(){
+        LocalDateTime time = LocalDateTime.now();
+
+        int month = time.getMonthValue();
+        if(month < 9){
+            return LocalDateTime.of(time.getYear() - 1, 9, 1, 0, 0);
+        }
+
+        else {
+            return LocalDateTime.of(time.getYear(), 9, 1, 0, 0);
+        }
+
+    }
+
+}
