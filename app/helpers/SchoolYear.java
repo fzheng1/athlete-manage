be90efc298e4dd6571b7215f972eae74a1c99f52
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-05-15 14:33:45 -0400

    Add SchoolYear helper class.

---
+++ app/helpers/SchoolYear.java
@@ -0,0 +1,32 @@
+package helpers;
+
+import java.time.LocalDateTime;
+
+public class SchoolYear {
+
+    public static LocalDateTime currentSchoolYear(){
+        return toSchoolYear(LocalDateTime.now());
+    }
+
+    public static LocalDateTime toSchoolYear(LocalDateTime dateTime) {
+        if(dateTime.getMonthValue() < 9)
+            return LocalDateTime.of(dateTime.getYear()-1, 9,1,0,0,0);
+        return LocalDateTime.of(dateTime.getYear(), 9, 1, 0, 0);
+    }
+
+    public static LocalDateTime toSchoolYear(int year, int month, int dayOfMonth){
+        LocalDateTime dateTime = LocalDateTime.of(year, month, dayOfMonth, 0, 0, 0);
+        return toSchoolYear(dateTime);
+    }
+
+    public static String formatSchoolYear(int year, int month, int dayOfMonth){
+        LocalDateTime dateTime = LocalDateTime.of(year, month, dayOfMonth, 0, 0, 0);
+        return formatSchoolYear(dateTime);
+    }
+
+    public static String formatSchoolYear(LocalDateTime dateTime) {
+        if(dateTime.getMonthValue() < 9)
+            return (dateTime.getYear() - 1) + "-" + (dateTime.getYear());
+        return (dateTime.getYear()) + "-" + (dateTime.getYear() + 1);
+    }
+}
---
+++ test/helpers/SchoolYearTest.java
@@ -0,0 +1,63 @@
+package helpers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import java.time.LocalDateTime;
+
+public class SchoolYearTest {
+
+    @Test
+    public void testCurrentSchoolYear(){
+        LocalDateTime currentSchoolYear = SchoolYear.currentSchoolYear();
+        LocalDateTime now = LocalDateTime.now();
+        int month = now.getMonthValue();
+        int year =  month < 9 ? now.getYear() - 1 : now.getYear();
+
+        assertEquals((LocalDateTime.of(year, 9,1,0,0,0)),currentSchoolYear);
+    }
+
+    @Test
+    public void testOctoberSchoolYearShouldBeSameYear() {
+        LocalDateTime october = LocalDateTime.of(2017, 10, 1, 0, 0, 0);
+        LocalDateTime schoolYear = SchoolYear.toSchoolYear(october);
+        assertEquals("October's school year should be september of the same year", LocalDateTime.of(2017, 9, 1, 0, 0, 0), schoolYear);
+    }
+
+    @Test
+    public void testJanuarySchoolYearShouldBePreviousYear() {
+        LocalDateTime january = LocalDateTime.of(2017, 1, 1, 0, 0, 0);
+        LocalDateTime schoolYear = SchoolYear.toSchoolYear(january);
+        assertEquals("January's school year should be september of the previous year", LocalDateTime.of(2016, 9, 1, 0, 0, 0), schoolYear);
+    }
+
+
+    @Test
+    public void testToSchoolYear(){
+        LocalDateTime time = SchoolYear.toSchoolYear(2017, 10, 4);
+        assertEquals(LocalDateTime.of(2017, 9,1,0,0,0), time);
+    }
+
+    @Test
+    public void testFormatSchoolYear(){
+        String schoolYear = SchoolYear.formatSchoolYear(2018, 3, 4);
+        assertEquals("2017-2018", schoolYear);
+    }
+
+    @Test
+    public void testFormatSchoolYearOctober(){
+        LocalDateTime october = LocalDateTime.of(2017, 10, 1, 0, 0, 0);
+        String result = SchoolYear.formatSchoolYear(october);
+        assertEquals("October '17 should be rendered as '2017-2018'", "2017-2018", result);
+    }
+
+    @Test
+    public void testFormatSchoolYearApril(){
+        LocalDateTime april = LocalDateTime.of(2018, 4, 1, 0, 0, 0);
+        String result = SchoolYear.formatSchoolYear(april);
+        assertEquals("April '18 should be rendered as '2017-2018'", "2017-2018", result);
+    }
+
+
+}
+
